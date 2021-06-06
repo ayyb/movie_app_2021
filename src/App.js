89ad7ab2b688233fc6c24f09b8f4859fad2e733d
ayyb <ayyb@kakao.com>
2021-06-06 14:24:33 +0900

    life cycle apply

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    count:0
-  };
-  add(){
-    console.log('add')
-  }
-  minus(){
-    console.log('minus')
-  }
   render(){
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add()}>Add</button>
-      <button onClick={this.minus()}>minus</button>
-    </div>;
+    console.log("I am rendering")
+    return <div />;
   }
 }
 
