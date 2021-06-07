2a7b47d32d5325d1dffb4c345f31fdd4238ee881
ayyb <ayyb@kakao.com>
2021-06-08 01:54:38 +0900

    component / router apply

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import { HashRouter,Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Naviagtion from "./components/Navigation";
+import "./App.css"
 
 function App(){
   return <HashRouter>
+    <Naviagtion />
      <Route path="/" exact={true} component={Home} />
      <Route path="/about" component={About} />
+     <Route path="/movie/:id" component={Detail} />
   </HashRouter>
 }
 
